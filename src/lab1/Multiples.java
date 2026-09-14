10e9215ba9cda76084276c8298416fb7c9cfaa73
Mostafa <168289585+iamequa@users.noreply.github.com>
2026-09-14 16:51:35 -0400

    multiples refactoring

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,10 +2,15 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int total = multiples();
+        System.out.println(total);
+    }
+
+    private static int multiples() {
         int total = 0;
         for (int i = 1; i < 1000; i++){
             if (i % 3 == 0 | i % 5 == 0) total++;
         }
-        System.out.println(total);
+        return total;
     }
 }
