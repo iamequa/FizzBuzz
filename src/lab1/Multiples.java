667f57079b54058b405f176c9ff78c001b27260f
Mostafa <168289585+iamequa@users.noreply.github.com>
2026-09-14 16:45:21 -0400

    multiples thing

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,11 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0 | i % 5 == 0) total++;
+        }
+        System.out.println(total);
+    }
+}
