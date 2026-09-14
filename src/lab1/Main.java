00358e14920ecc533e3e4f080b3c2bee89ab30c4
Mostafa <168289585+iamequa@users.noreply.github.com>
2026-09-14 16:32:48 -0400

    Add print statement for GitHub pull test
    yes

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
         FizzBuzz.main(args);
+        System.out.println("Trying to pull from GitHub");
     }
 }
